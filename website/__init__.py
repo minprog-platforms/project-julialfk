424d2a0f5e3da02e2f55b9c135c0bcff308615eb
julialfk <j.liemfk@gmail.com>
2021-12-10 14:32:06 +0100

    MVP first upload

--- website/__init__.py
+++ website/__init__.py
@@ -6,10 +6,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'key'
 
     from .views import views
-    from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
-
 
     return app
--- website/views.py
+++ website/views.py
@@ -1,8 +1,32 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from webbrowser import open_new_tab
+
+from .models import Random_restaurant
 
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        location = request.form.get('location')
+        rating = request.form.get('rating')
+
+        if len(location) < 2:
+            flash("Location must be longer than 1 character.", category='error')
+        elif bool(rating) == False:
+            flash("Please enter a minimum rating.", category='error')
+        else:
+            search = Random_restaurant(location, rating)
+            current_coords = search.current_coords()
+            restaurant_ids = search.nearby_search(current_coords, search._rating)
+
+            if len(restaurant_ids) < 1:
+                flash("No nearby restaurants found with these conditions.\nPlease try again with a different input.", category='error')
+            else:
+                restaurant_url = search.randomize(restaurant_ids)
+                flash(f"Restaurant found!", category='success')
+
+                open_new_tab(restaurant_url)
+
     return render_template("home.html")
